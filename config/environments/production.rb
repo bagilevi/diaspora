c282745953f66035b1349a4f6c74439e781799e6
Levente Bagi <bagilevi@gmail.com>
2012-05-06 13:14:53 +0100

    compile and serve assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Diaspora::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
@@ -50,7 +50,7 @@ Diaspora::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
